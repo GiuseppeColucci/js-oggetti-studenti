098714820adb635e92cbb763b15152dab86bcd75
giuseppecolucci <giuseppecolucciwork@gmail.com>
2021-01-18 16:59:21 +0100

    finito da migliorare

--- js/main.js
+++ js/main.js
@@ -7,17 +7,46 @@
  */
 //prima parte
 var html=document.getElementById('studente-1')
-var studente={
+var scolare=[{
     'nome':'marco',
     'congmome':'Martinez',
     'eta': 34,
     
-}
+}];
 
-//da capire!!!
-for(var key in studente)  {
+for(var key =0; key< scolare.length; key++)  {
+    var studente=scolare[key]
     // console.log(studente.congmome)
-    html.innerHTML=studente[key]
+    html.innerHTML=studente.nome + studente.congmome + studente.eta;
+    console.log(studente);
+ 
+};
+
    
+ //seconda parte
+ 
+ var classeStudenti=[
+     {'nome':'paolo','cognome':'papero','età':45},
+     {'nome':'pippa','cognome':'middelton','età':65},
+
+ ];
+
+
+
+ for(var k =0; k< classeStudenti.length; k++)  {
+    var alunno=classeStudenti[k]
+    // console.log(studente.congmome)
+    alert(alunno.nome + alunno.cognome );
     
-  }
+ 
+};
+
+var nuovoStudenteInserito={}
+
+nuovoStudenteInserito.nome=prompt('inserire il nome del nuovo studente');
+nuovoStudenteInserito.cognome=prompt('inserire il cognome del nuovo studente');
+nuovoStudenteInserito.eta=prompt("inserire l'età del nuovo studente");
+
+
+classeStudenti.push(nuovoStudenteInserito)
+console.log(classeStudenti)
