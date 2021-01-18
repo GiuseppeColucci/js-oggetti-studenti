8fbd9f4d23a31b38fc1459ec90575f299d58a41c
giuseppecolucci <giuseppecolucciwork@gmail.com>
2021-01-18 16:01:11 +0100

    primo studente

--- js/main.js
+++ js/main.js
@@ -1,5 +1,23 @@
 /**
  * Creare un oggetto che descriva uno studente con le seguenti proprietà: nome, cognome e età. Stampare a schermo attraverso il for tutte le proprietà.
+ * 
+ * 
     Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno nome e cognome
     Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età. 
  */
+//prima parte
+var html=document.getElementById('studente-1')
+var studente={
+    'nome':'marco',
+    'congmome':'Martinez',
+    'eta': 34,
+    
+}
+
+//da capire!!!
+for(var key in studente)  {
+    // console.log(studente.congmome)
+    html.innerHTML=studente[key]
+   
+    
+  }
